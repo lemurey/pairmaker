7885903783d747674c3369fc01523cec4b1db264
lemurey <lee.murray@gmail.com>
2016-07-27 12:01:38 -0700

    refactor and update pairmaker, now divides triples evenly and separates them temporally

--- pairmaker.py
+++ pairmaker.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle, choice
 from collections import Counter
 import datetime as dt
 
@@ -27,12 +27,16 @@ class PairMaker(object):
         self.dows = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
         self.counts = Counter()
         self.all_pairs = None
+        self.all_named_pairs = None
         self.out_dict = None
+        self.cur_trip = None
+        self.triples = None
+        self.problem = 0
         if students:
             self._load_student_file(students)
         else:
             self.students = None
-
+        self._prep()
 
     def _set_start_date(self, date):
         date = dt.datetime.strptime(date,'%m/%d/%Y')
@@ -45,13 +49,11 @@ class PairMaker(object):
                 student = line.strip()
                 students.append(student)
         self._setup_from_num(len(students))
-        if self.odd_num:
-            students.append(' ')
         shuffle(students)
+        self.empty_index = -1
         if self.odd_num:
-            self.empty_index = students.index(' ')
-        else:
-            self.empty_index = -1
+            students = [' '] + students
+            self.empty_index = 0
         self.students = students
 
     def _setup_from_num(self, num):
@@ -82,14 +84,79 @@ class PairMaker(object):
         row2 = index[:-(self.row_length+1):-1]
         return zip(row1,row2)
 
-    def _make_output_dict(self):
+    def _make_named_list(self):
+        all_named_pairs = []
+        solo = None
+        for pairs in self.all_pairs:
+            cur_pairs = []
+            for pair in pairs:
+                if self.empty_index in pair:
+                    solo = pair[1]
+                    continue
+                if solo:
+                    pair = (pair[0], pair[1], solo)
+                    solo = None
+                pair = tuple(sorted([self.students[i] for i in pair]))
+                cur_pairs.append(pair)
+            all_named_pairs.append(cur_pairs)
+        self.all_named_pairs = all_named_pairs
+
+    def _prep(self):
+        self._make_index_lists()
+        if self.students:
+            self._make_named_list()
+            self.all_pairs = self.all_named_pairs
+        self._make_triples()
+        if self.triples:
+            self._reorder()
+        else:
+            shuffle(self.all_pairs)
+
+    def _reorder(self, count = 0):
+        first_triple = self.triples[0]
+        output = [0]
+        last_triple = None
+        n = len(self.triples)
+        inds = range(1,n)
+        iters = 0
+        while len(output) != n:
+            iters += 1
+            if iters > 500:
+                break
+            ind = choice(inds)
+            check = self.triples[ind]
+            if self._check_trips(check, last_triple):
+                output.append(ind)
+                inds.remove(ind)
+                last_triple = check
+            elif self._check_trips(check, first_triple) and last_triple:
+                output = [ind] + output
+                inds.remove(ind)
+                first_triple = check
+        if len(output) != n:
+            return self._reorder(count+1)
+        new_trips = []
+        new_pairs = []
+        for i in output:
+            new_trips.append(self.triples[i])
+            new_pairs.append(self.all_pairs[i])
+        self.all_pairs = new_pairs
+        self.triples = new_trips
+        self.count = count
+
+    def make_output_dict(self):
+        '''
+        a dictionary with keys of weeks and values of a dictionary
+        with keys of days and values of the pair assignments for that day
+        INPUT: None
+        OUTPUT: None
+        '''
         out_dict = {}
-        if not self.all_pairs:
-            self._make_index_lists()
         cur_day = self.start_date
         cur_week = 0
         cur_dict = {}
-        while len(self.all_pairs) > 0:
+        pulling = self.all_pairs[:]
+        while pulling:
             if cur_day.weekday() > 4:
                 cur_day = cur_day + dt.timedelta(1)
                 continue
@@ -97,7 +164,7 @@ class PairMaker(object):
                 cur_week += 1
                 out_dict['week {}'.format(cur_week)] = cur_dict
                 cur_dict = {}
-            cur_pair = self.all_pairs.pop()
+            cur_pair = pulling.pop()
             shuffle(cur_pair)
             cur_dict[cur_day.strftime("%A")] = cur_pair
             cur_day = cur_day + dt.timedelta(1)
@@ -107,53 +174,36 @@ class PairMaker(object):
     def make_md_tables(self, out_path = 'pairs.md'):
         '''
         creates a markdown file of the student pairs
-        out_path: string, output path to write markdown tables to
+        INPUT: out_path, a string, output path to write markdown tables to
         '''
         if not self.out_dict:
-            self._make_output_dict()
+            self.make_output_dict()
         weeks = sorted(self.out_dict.keys())
-        if self.students:
-            write_students = True
-        else:
-            write_students = False
-        triples = []
         with open(out_path,'w') as f:
             for week in weeks:
                 w_str = '{}\n\n|day of week|groups|\n|---|---|\n'.format(week)
                 all_days = self._make_week(self.out_dict[week].keys())
-                solo = None
                 for day in all_days:
-                    pairs = []
-                    for pair in self.out_dict[week][day]:
-                        if self.empty_index in pair:
-                            ind = 1 - pair.index(self.empty_index)
-                            solo = (pair[ind],)
-                        else:
-                            pairs.append(pair)
-                    if solo:
-                        pairs = self._add_single(pairs,solo)
-                    if write_students:
-                        pairs = [tuple(self.students[i] for i in pair) for pair in pairs]
+                    pairs = self.out_dict[week][day]
                     w_str += '|{}|{}|\n'.format(day, ',<br>'.join(str(x) for x in pairs))
                 f.write(w_str)
             f.write('\n')
 
-    def _add_single(self, pairs, solo):
-        output = set()
-        to_add = True
-        add = False
-        vals = Counter()
-        for pair in pairs:
-            val = self.counts[pair[0]] + self.counts[pair[1]]
-            vals[pair] = val
-            output.add(pair)
-        update = vals.most_common()[-1][0]
-        output.remove(update)
-        self.counts[update[0]] += 1
-        self.counts[update[1]] += 1
-        new_entry = (update[0], update[1], solo[0])
-        output.add(new_entry)
-        return list(output)
+    def _check_trips(self, trip1, trip2):
+        if trip2 is None:
+            trip2 = self.triples[0]
+        for entry in trip1:
+            if entry in trip2:
+                return False
+        return True
+
+    def _make_triples(self):
+        triples = []
+        for pairs in self.all_pairs:
+            for pair in pairs:
+                if len(pair) == 3:
+                    triples.append(pair)
+        self.triples = triples
 
     def _make_week(self,lst):
         output = []
@@ -169,8 +219,55 @@ def make_fake_students(num_students = 19):
             file.write(alphabet[i] + '\n')
 
 
-if __name__ == '__main__':
-    make_fake_students(26)
-    pm = PairMaker(start_date = '08/01/2016',students = 'students.txt')
-    pm.make_md_tables()
+def main(n):
+    
+    iters = 5000
+    counts = []
+    make_fake_students(n)
+    for _ in range(iters):
+        pm = PairMaker(start_date = '08/01/2016',students = 'students.txt')
+        pm.make_md_tables()
+        if pm.count > 0:
+            counts.append(pm.count)
+    print sorted(counts, reverse=True)
+    print len(counts)
+    # for _ in range(iters):
+        
+    #     c = Counter()
+    #     c1 = Counter()
+    #     prev = None
+    #     for pairs in pm.all_pairs:
+    #         for pair in pairs:
+    #             c1[pair] += 1
+    #     for t in pm.triples:
+    #         if not prev:
+    #             prev = t
+    #             continue
+    #         for entry in t:
+    #             if entry in prev:
+    #                 c[entry] += 1
+    #         prev = t
+    #     if len(c) > 0:
+    #         count += 1
+    #     if pm.count > 0:
+    #         problems += 1
+    #     if c1.most_common(1)[0][1] > 1:
+    #         multiples += 1
+    #     if len(c1) != 300:
+    #         weird += 1
+    # print 'number of invalid lists: {}'.format(count)
+    # print 'number of non-convergent solutions: {}'.format(problems)
+    # print 'number of pair multiples: {}'.format(multiples)
+    # print 'number of werid pair lists: {}'.format(weird)
+        # for k,v in c.iteritems():
+        #     print '{}:{}'.format(k,v)
+    
+    # for t in new:
+    #     print t
 
+
+
+
+if __name__ == '__main__':
+    n = 25
+    main(n)
